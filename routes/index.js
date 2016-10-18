88bb801d292980a84fdfa39a1f6ae22c7958e9fe
YzpHelios <1229102737@qq.com>
2016-10-18 17:46:37 +0800

    A simple blog system.

---
+++ routes/index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
+var router = express.Router();
+
+
+router.get('/', function (req, res) {
+    res.render('index', { user : req.user,title:'主页' });
+});
+
+router.get('/register', function(req, res) {
+    res.render('register', {user : req.user, error : req.flash('error'),title:'注册' });
+});
+
+router.post('/register', function(req, res, next) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+          return res.render('register', { error : err.message ,title:'注册',user : req.user});
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            req.session.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect('/');
+            });
+        });
+    });
+});
+
+
+router.get('/login', function(req, res) {
+    res.render('login', { user : req.user, error : req.flash('error'),title:'登陆'});
+});
+
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), function(req, res, next) {
+    req.session.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
+router.get('/logout', function(req, res, next) {
+    req.logout();
+    req.session.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
+router.get('/ping', function(req, res){
+    res.status(200).send("pong!");
+});
+
+module.exports = router;
